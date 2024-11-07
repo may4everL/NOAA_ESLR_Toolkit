a394f0d6c8cfe5ddf6a7abd92b201f3b1f13c064
Wei Sun <ws1097@usnh.edu>
2024-11-07 16:44:28 -0500

    Remove github icon and link

--- Effect_of_Sea_Level_Rise_on_Pavement.py
+++ Effect_of_Sea_Level_Rise_on_Pavement.py
@@ -38,12 +38,18 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 # Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 # image = Image.open('Figure 3.jpg')
 # st.image(image, caption = 'Schematics for the pavement modelling')
