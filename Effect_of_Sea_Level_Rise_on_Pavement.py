1f69a8d6eacb69a6e95a2d41663708f3f8614bb4
Wei Sun <ws1097@usnh.edu>
2025-02-03 11:08:45 -0500

    New push to make online version work

--- Effect_of_Sea_Level_Rise_on_Pavement.py
+++ Effect_of_Sea_Level_Rise_on_Pavement.py
@@ -757,7 +757,7 @@ else:
                         xanchor='center',  # Anchors the center of the legend at x
                         yanchor='top',  # Anchors the top of the legend at y
                         orientation='h'  # Optional: makes the legend horizontal)
-                    ))
+                    )))
     fig.update_xaxes(tickfont=dict(size=20),title_font=dict(size=20), range=[-1, design_years+1])
     fig.update_yaxes(tickfont=dict(size=20),title_font=dict(size=20))
     st.plotly_chart(fig, use_container_width=True)
